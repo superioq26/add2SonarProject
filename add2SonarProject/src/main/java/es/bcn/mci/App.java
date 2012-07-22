0f356092ec1c036532c51045cf96eafbf220a8c4
Ivan <iortiz@bcn.cat>
2012-07-22 21:12:06 +0200

    Prueba

---
+++ add2SonarProject/src/main/java/es/bcn/mci/App.java
@@ -0,0 +1,13 @@
+package es.bcn.mci;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
